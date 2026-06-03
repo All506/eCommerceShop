fbe5145208ca2243f38a4b13dc9d937eba1b69fe
Allan Trejos <allcincoceroseis@gmail.com>
2026-06-03 01:06:41 -0600

    Sale rendering

--- front/src/components/ShoeCard/ShoeCard.tsx
+++ front/src/components/ShoeCard/ShoeCard.tsx
@@ -8,12 +8,27 @@ interface Shoe {
     img: string
 }
 
-function ShoeCard({shoe}: Shoe) {
+function ShoeCard({ shoe }: Shoe) {
     return (
         <div className={styles.cardBackground}>
-        <img src={shoe.img}></img>
-        <span className={styles.model}>{shoe.brand} {shoe.model}</span>
-        <span className={styles.price}>€{shoe.price}</span>
+            <img src={shoe.img}></img>
+            <span className={styles.model}>{shoe.brand} {shoe.model}</span>
+
+            {shoe.sale ? (
+                <span className={styles.sale}>
+                    <span className={styles.price}>
+                        €{shoe.price}
+                    </span> 
+                    <br></br>
+                    €{(shoe.price * (1 - shoe.sale / 100)).toFixed(0)} 
+                    <br></br>
+                    <span>{shoe.sale}% off</span> 
+                </span>
+            ) : (
+                <span className={styles.price}>
+                    €{shoe.price}
+                </span>
+            )}
         </div>
     )
 }
