cc2a9d7c546eef9ab079f5bcabdac55fc3b9b779
Allan Trejos <allcincoceroseis@gmail.com>
2026-09-15 13:03:05 -0600

    Cambios en main

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -4,16 +4,13 @@ import App from './App';
 import './i18n';
 
 async function enableMocking() {
-  /*if (import.meta.env.MODE !== 'development') {
-    return;
-  }*/
-
   const { worker } = await import('./mocks/browser');
 
   await worker.start({
     serviceWorker: {
       url: `${import.meta.env.BASE_URL}mockServiceWorker.js`,
     },
+    onUnhandledRequest: 'bypass',
   });
 }
 
